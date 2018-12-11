9c75444f8b36ab78ac2fd37694033e03c6a97816
Fairy <fairyoo@qq.com>
2018-12-12 00:31:13 +0800

    pod install

---
+++ fairynews/Pods/Target Support Files/Pods-fairynews/Pods-fairynews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fairynews : NSObject
+@end
+@implementation PodsDummy_Pods_fairynews
+@end
---
+++ fairynews/Pods/Target Support Files/Pods-fairynewsUITests/Pods-fairynewsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fairynewsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_fairynewsUITests
+@end
